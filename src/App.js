ec5f7e048878525951f874f420dc77908f0cfd7f
Vivek R <r.vivek@kloctechnologies.co.in>
2020-02-20 19:01:15 +0530

    Demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'react',
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/todos`)
+        .then((res) => {
+            console.log(res);
+            console.log(res.data);
+
+            let data = res.data;
+
+            this.setState({ data });
+            console.log('state data', this.state.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    handleOnChangeEvent = (event) => {
+        console.log(event.target.value);
+
+        var value = event.target.value;
+
+        this.setState({ name: value });
+    }
+
+    render() {
+        console.log('render will be called');
+
+        let { data } = this.state;
+
+        return (
+            <div className="App">
+                We are now using the class component!
+                <h1>I'm {this.state.name}</h1>
+                <input type="text" onChange={this.handleOnChangeEvent} />
+                <ul>
+                    {
+                        data.map((item,index) => {
+                            return (
+                                <li key={index}>{item.title}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default App;
